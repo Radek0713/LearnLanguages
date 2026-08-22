ac86209948ec32f24771faefefd70e93fc590bf7
Radek Stałęga <rstalega13@gmail.com>
2026-08-22 11:49:21 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LearnLanguages
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
