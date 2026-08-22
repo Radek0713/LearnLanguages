bb959d6489c8b6e405895a06708d6e9438a8ee81
Radek Stałęga <rstalega13@gmail.com>
2026-08-22 14:07:46 +0200

    Początek
    Menu start, przyciski

---
+++ Views/Controls/StartMenu.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LearnLanguages.Views;
+
+namespace LearnLanguages.Views.Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy StartMenu.xaml
+    /// </summary>
+    public partial class StartMenu : UserControl
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+        private void Btn_english_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Window.GetWindow(this) as MainWindow;
+            if (mainWindow != null)
+            {
+                mainWindow.MainContentArea.Content = new LanguageMenu();
+            }
+        }
+        /*
+        private void Btn_german_Click(object sender, RoutedEventArgs e) { }
+        private void Btn_french_Click(object sender, RoutedEventArgs e) { }
+        private void Btn_spanish_Click(object sender, RoutedEventArgs e) { }
+        private void Btn_italian_Click(object sender, RoutedEventArgs e) { }
+        */
+
+        private void Btn_unsupportedLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            string languageName = (button?.Content?.ToString() ?? "Ten język").ToLower();
+
+            MessageBox.Show(
+                $"Język {languageName} nie jest obecnie obsługiwany w tej wersji aplikacji.",
+                "Brak obsługi języka",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information
+            );
+        }
+    }
+}
--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LearnLanguages
+namespace LearnLanguages.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -19,6 +19,7 @@ namespace LearnLanguages
         public MainWindow()
         {
             InitializeComponent();
+            MainContentArea.Content = new Controls.StartMenu();
         }
     }
 }
